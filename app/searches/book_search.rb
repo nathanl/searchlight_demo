2c128a8f340ae238ad599a6e14cda949337f8ed2
Nathan Long <nathanmlong@gmail.com>
2013-06-18 07:15:58 -0400

    Search by other category in which author wrote

--- app/searches/book_search.rb
+++ app/searches/book_search.rb
@@ -2,7 +2,7 @@ class BookSearch < Searchlight::Search
 
   search_on Book
 
-  searches :title, :category_id, :author_name_like
+  searches :title, :category_id, :author_name_like, :author_also_wrote_in_category_id
 
   def search_title
     search.where(title: title)
@@ -14,6 +14,15 @@ class BookSearch < Searchlight::Search
 
   def search_author_name_like
     search.joins(:author).where("concat(authors.first_name, ' ', authors.last_name) ILIKE ?", "%#{author_name_like}%")
+  end
+
+  def search_author_also_wrote_in_category_id
+    query_string = <<-YE_OLDE_QUERY_LANGUAGE.strip_heredoc
+    INNER JOIN authors                        ON books.author_id         = authors.id
+    INNER JOIN books      AS other_books      ON other_books.author_id   = authors.id
+    INNER JOIN categories AS other_categories ON other_books.category_id = other_categories.id
+    YE_OLDE_QUERY_LANGUAGE
+    search.joins(query_string).where("other_categories.id = ?", author_also_wrote_in_category_id)
   end
 
 end
