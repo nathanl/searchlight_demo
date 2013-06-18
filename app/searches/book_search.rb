551418cade9fa51a0805f9184e0df16b983b12ec
Nathan Long <nathanmlong@gmail.com>
2013-06-18 07:13:29 -0400

    Add search by `author_name_like`

--- app/searches/book_search.rb
+++ app/searches/book_search.rb
@@ -2,7 +2,7 @@ class BookSearch < Searchlight::Search
 
   search_on Book
 
-  searches :title, :category_id
+  searches :title, :category_id, :author_name_like
 
   def search_title
     search.where(title: title)
@@ -10,6 +10,10 @@ class BookSearch < Searchlight::Search
 
   def search_category_id
     search.joins(:category).where("categories.id = ?", category_id)
+  end
+
+  def search_author_name_like
+    search.joins(:author).where("concat(authors.first_name, ' ', authors.last_name) ILIKE ?", "%#{author_name_like}%")
   end
 
 end
