c3d26d6c17aed1037f15d4e4b47e26a70453f21c
Nathan Long <nathanmlong@gmail.com>
2015-09-26 05:38:32 -0400

    Upgrade Rails

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
-ruby "2.1.5"
+ruby "2.1.2"
 
-gem 'rails',          '~> 4.1.0'
+gem 'rails',          '~> 4.2.1'
 
 gem 'pg',             '~> 0.15'
 gem 'searchlight',    '~> 3.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -9,22 +9,25 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601141032) do
+ActiveRecord::Schema.define(version: 20130601141032) do
 
-  create_table "authors", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
   end
 
-  create_table "books", :force => true do |t|
+  create_table "books", force: :cascade do |t|
     t.string  "title"
     t.integer "author_id"
     t.integer "category_id"
   end
 
-  create_table "categories", :force => true do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
   end
 
